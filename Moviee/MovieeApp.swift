f0e2f4faa187911e7bc3644ea616d94ef6da218a
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-27 06:28:40 +0200

    Initial commit

---
+++ Moviee/MovieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieeApp.swift
+//  Moviee
+//
+//  Created by NomoteteS on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
